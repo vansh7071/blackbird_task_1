21b3ff3c8170b212f172178d06476b73a1a59678
Vansh Malhotra <vanshmalhotra7071@gmail.com>
2023-08-15 17:49:51 +0530

    FEAT: Updated-logo

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">
+          Or you can visit <a
+          className="App-link"
+          href="https://www.propelleraero.com/dirtmate/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here.
+        </a>
+        </p>
+
       </div>
     );
   }
